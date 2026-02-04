568ca85284464065d399361ca8cd1b636d451ee1
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-02-04 11:36:21 -0800

    Custom-Auth-View part 2

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/utils/assets.dart';
+import 'widgets/custom_auth_view.dart';
+
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
   static const String routeName = 'login-view';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomAuthView(
+        image: Assets.imagesLoginImage,
+        showBackIcon: false,
+        showHand: true,
+        title: 'أهلا بك .. جاهز للمغامرة',
+        subtitle: 'سجّل دخولك علشان تستعرض رحلات السفاري، تحجز مغامرتك، وتدير حجوزاتك بكل سهولة.',
+        note: 'سجّل حسابك لإدارة وتشغيل منصة رحلات السفاري في واحة سيوة بكفاءة واحترافية',
+        form: SizedBox(),
+      ),
+    );
   }
 }
