589173c1c64b1fbd6a49334104efb182397822fd
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-02-04 10:08:41 -0800

    Init Localization using intl extension [ en & ar ].

--- lib/bedouin_trails.dart
+++ lib/bedouin_trails.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'generated/l10n.dart';
 
 class BedouinTrails extends StatelessWidget {
   const BedouinTrails({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Bedouin Trails', home: _TestPage());
+    return MaterialApp(
+      title: 'Bedouin Trails',
+      locale: const Locale('ar'),
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      home: _TestPage(),
+    );
   }
 }
 
