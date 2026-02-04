6fb3db6fe8fd6df181aa978f43299893b1a7ce0b
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-02-04 10:22:14 -0800

    Add the Colors of the dashboard.

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const white = Color(0xffFFFFFF);
+  static const whiteGrey = Color(0xffF5F5F5);
+  static const black = Color(0xff000000);
+  static const black2 = Color(0xff3A3A3A);
+  static const phosphorBlue = Color(0xffB0BAC2);
+  static const cyanGreen = Color(0xff00C628);
+  static const sandyBrown = Color(0xffCF9050);
+  static const borderGrey = Color(0xffC1C1C1);
+  static const blue = Color(0xff0077FF);
+}
