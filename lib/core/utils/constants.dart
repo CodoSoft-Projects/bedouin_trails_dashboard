10b6a71cc288d906032e51032d5088c015a2f790
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-02-07 09:02:02 -0800

    Add [Vexa, VexaLight] font family

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,3 +1,5 @@
 abstract class Constants {
   static const String elMessiriFontFamily = 'Tajawal';
+  static const String vexaFontFamily = 'VEXA';
+  static const String vexaLightFontFamily = 'VEXALight';
 }
