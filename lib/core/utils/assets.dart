0031d0e3686a8e53808e628e879ddf29b033719b
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-02-07 10:20:50 -0800

    Add some assets.

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -4,6 +4,8 @@ class Assets {
   
   static const String animationsHandGif = "assets/animations/hand_gif.gif";
 
+  static const String imagesCamelCircle = "assets/images/camel_circle.png";
+
   static const String imagesForgetImage = "assets/images/forget_image.png";
 
   static const String imagesLoginImage = "assets/images/login_image.png";
@@ -11,6 +13,8 @@ class Assets {
   static const String imagesLogo = "assets/images/logo.png";
 
   static const String imagesResetImage = "assets/images/reset_image.png";
+
+  static const String imagesTestTripImage = "assets/images/test_trip_image.png";
 
   static const String imagesTestUser = "assets/images/test_user.png";
 }
