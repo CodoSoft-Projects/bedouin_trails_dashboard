62913f4b0042c26ca0b8935f1a794b3a5d6ab3c4
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-02-07 14:23:21 -0800

    Show Trip Info Section part one.

---
+++ lib/features/trips/presentation/views/widgets/show_trip_info_section.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/app_colors.dart';
+import '../../../../../core/utils/app_text_styles.dart';
+import '../../../../../core/utils/constants.dart';
+import '../../../../../core/widgets/custom_button.dart';
+import '../../../../../core/widgets/custom_white_box.dart';
+
+class ShowTripInfoSection extends StatefulWidget {
+  const ShowTripInfoSection({super.key});
+
+  @override
+  State<ShowTripInfoSection> createState() => _ShowTripInfoSectionState();
+}
+
+class _ShowTripInfoSectionState extends State<ShowTripInfoSection> {
+  bool showInfo = true;
+
+  void toggleInfo(bool val) {
+    showInfo = val;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomWhiteBox(
+      showBorder: true,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                'بيانات الرحلة :',
+                style: AppTextStyles.regular24(
+                  context,
+                ).copyWith(fontFamily: Constants.vexaFontFamily),
+              ),
+              const Spacer(),
+              CustomWhiteBox(
+                color: AppColors.whiteGrey,
+                vPadding: 0,
+                hPadding: 0,
+                child: Row(
+                  children: [
+                    CustomButton(
+                      text: 'بيانات الرحلة',
+                      color: showInfo
+                          ? AppColors.sandyBrown
+                          : AppColors.whiteGrey,
+                      textColor: showInfo ? AppColors.white : AppColors.black,
+                      onPressed: () => toggleInfo(true),
+                    ),
+
+                    CustomButton(
+                      text: 'برنامج الرحلة',
+                      color: !showInfo
+                          ? AppColors.sandyBrown
+                          : AppColors.whiteGrey,
+                      textColor: !showInfo ? AppColors.white : AppColors.black,
+                      onPressed: () => toggleInfo(false),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 12),
+        ],
+      ),
+    );
+  }
+}
