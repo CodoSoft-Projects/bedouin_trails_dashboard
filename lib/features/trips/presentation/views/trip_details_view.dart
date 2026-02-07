ba653905d174ccc3a6971ad90f28e980429b3b17
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-02-07 15:47:57 -0800

    Trip Details View UI done.

--- lib/features/trips/presentation/views/trip_details_view.dart
+++ lib/features/trips/presentation/views/trip_details_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/utils/size_config.dart';
 import '../../../../core/widgets/custom_app_bar.dart';
 import 'widgets/show_trip_images_section.dart';
 import 'widgets/show_trip_info_section.dart';
@@ -11,6 +12,7 @@ class TripDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var isMobile = SizeConfig.isMobile();
     return Scaffold(
       appBar: customAppBar(context, title: 'تفاصيل الرحلة', showBack: true),
       body: CustomScrollView(
@@ -21,19 +23,40 @@ class TripDetailsView extends StatelessWidget {
           SliverToBoxAdapter(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 12.0),
-              child: Row(
-                spacing: 12,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(child: ShowTripImagesSection()),
-                  Expanded(child: ShowTripInfoSection()),
-                ],
-              ),
+              child: isMobile ? const _Mobile() : const _Desktop(),
             ),
           ),
           const SliverToBoxAdapter(child: SizedBox(height: 12)),
         ],
       ),
+    );
+  }
+}
+
+class _Desktop extends StatelessWidget {
+  const _Desktop();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 12,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(child: ShowTripImagesSection()),
+        Expanded(child: ShowTripInfoSection()),
+      ],
+    );
+  }
+}
+
+class _Mobile extends StatelessWidget {
+  const _Mobile();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 12,
+      children: const [ShowTripImagesSection(), ShowTripInfoSection()],
     );
   }
 }
