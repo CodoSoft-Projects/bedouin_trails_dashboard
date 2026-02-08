867258019ede17af760de4083f63d82f2c37248d
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-02-08 09:20:02 -0800

    add images section to update-trip-view.

--- lib/features/trips/presentation/views/update_trip_view.dart
+++ lib/features/trips/presentation/views/update_trip_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/utils/size_config.dart';
 import '../../../../core/widgets/custom_app_bar.dart';
+import 'widgets/update_trip_images_section.dart';
 
 class UpdateTripView extends StatelessWidget {
   const UpdateTripView({super.key});
@@ -8,12 +10,53 @@ class UpdateTripView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var isMobile = SizeConfig.isMobile();
     return Scaffold(
       appBar: customAppBar(
         context,
         title: 'تعديل تفاصيل الرحلة',
         showBack: true,
       ),
+      body: CustomScrollView(
+        slivers: [
+          const SliverToBoxAdapter(child: SizedBox(height: 12)),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: isMobile ? const _Mobile() : const _Desktop(),
+            ),
+          ),
+          const SliverToBoxAdapter(child: SizedBox(height: 12)),
+        ],
+      ),
+    );
+  }
+}
+
+class _Desktop extends StatelessWidget {
+  const _Desktop();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 12,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(child: UpdateTripImagesSection()),
+        Expanded(child: SizedBox()),
+      ],
+    );
+  }
+}
+
+class _Mobile extends StatelessWidget {
+  const _Mobile();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 12,
+      children: const [UpdateTripImagesSection(), SizedBox()],
     );
   }
 }
---
+++ lib/features/trips/presentation/views/widgets/update_trip_images_section.dart
@@ -0,0 +1,73 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/app_colors.dart';
+import '../../../../../core/utils/app_text_styles.dart';
+import '../../../../../core/utils/constants.dart';
+import '../../../../../core/widgets/custom_button.dart';
+import '../../../../../core/widgets/custom_white_box.dart';
+import 'trip_image.dart';
+
+class UpdateTripImagesSection extends StatelessWidget {
+  const UpdateTripImagesSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomWhiteBox(
+      showBorder: true,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                'صور الرحلة :',
+                style: AppTextStyles.regular24(
+                  context,
+                ).copyWith(fontFamily: Constants.vexaFontFamily),
+              ),
+              const Spacer(),
+
+              CustomButton(
+                text: 'إضافة صورة',
+                color: AppColors.sandyBrown,
+                onPressed: () {},
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          _ImagesGridView(),
+        ],
+      ),
+    );
+  }
+}
+
+class _ImagesGridView extends StatelessWidget {
+  const _ImagesGridView();
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        var width = constraints.maxWidth;
+        var crossAxisCount = (width / 160).toInt();
+        return GridView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: max(crossAxisCount, 1),
+            mainAxisSpacing: 12.0,
+            crossAxisSpacing: 12.0,
+            childAspectRatio: 1,
+            mainAxisExtent: 160,
+          ),
+          itemCount: 20,
+          itemBuilder: (context, index) {
+            return TripImage(canDelete: true);
+          },
+        );
+      },
+    );
+  }
+}
