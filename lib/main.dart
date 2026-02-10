fbb4e7bd93f163d65e8ebf66230933f21e6bdb7b
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-02-10 09:34:37 -0800

    Initialize Get It

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 
 import 'bedouin_trails.dart';
 import 'core/functions/open_hive_boxes.dart';
+import 'core/services/service_locator.dart';
 import 'core/utils/k_platform.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  // Initialize Get It
+  setupServicesLocator();
   // Open Hive Boxes
   await openHiveBoxes();
   runApp(const BedouinTrails());
