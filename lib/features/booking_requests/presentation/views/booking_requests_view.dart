d1694793e8c37d0c67e19658ff412cc7f614c426
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-02-11 09:00:29 -0800

    Add Booking-Requests-View to Booking-Request-View.

--- lib/features/booking_requests/presentation/views/booking_requests_view.dart
+++ lib/features/booking_requests/presentation/views/booking_requests_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'widgets/booking_request_view_header.dart';
+import 'widgets/booking_requests_grid_view.dart';
 
 class BookingRequestsView extends StatelessWidget {
   const BookingRequestsView({super.key});
@@ -11,6 +12,7 @@ class BookingRequestsView extends StatelessWidget {
       slivers: [
         const SliverToBoxAdapter(child: SizedBox(height: 12)),
         const BookingRequestViewHeader(),
+        SliverToBoxAdapter(child: const BookingRequestsGridView()),
       ],
     );
   }
---
+++ lib/features/booking_requests/presentation/views/widgets/booking_requests_grid_view.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import '../../../../trips/presentation/views/widgets/trip_card.dart';
+
+class BookingRequestsGridView extends StatelessWidget {
+  const BookingRequestsGridView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          var width = constraints.maxWidth;
+          var crossAxisCount = (width / 333).toInt();
+          return GridView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: max(crossAxisCount, 1),
+              mainAxisSpacing: 12.0,
+              crossAxisSpacing: 12.0,
+              childAspectRatio: 336 / 572,
+              mainAxisExtent: 500,
+            ),
+            itemCount: 20,
+            itemBuilder: (context, index) {
+              return TripCard(
+                onTripTap: () {
+                  // Navigator.of(context).pushNamed(TripDetailsView.routeName);
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
