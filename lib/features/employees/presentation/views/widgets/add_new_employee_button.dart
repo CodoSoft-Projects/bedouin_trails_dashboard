0b973e1cc9f2dc6fb58111bb674753736dc50626
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-02-16 11:34:05 -0800

    Apply adding new employee.

--- lib/features/employees/presentation/views/widgets/add_new_employee_button.dart
+++ lib/features/employees/presentation/views/widgets/add_new_employee_button.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../../../core/utils/app_colors.dart';
 import '../../../../../core/widgets/custom_button.dart';
+import '../../manager/employees_provider.dart';
 import '../new_employee_view.dart';
 
 class AddNewEmployeeButton extends StatelessWidget {
@@ -13,6 +15,7 @@ class AddNewEmployeeButton extends StatelessWidget {
       text: 'اضافة موظف جديد',
       color: AppColors.blue,
       onPressed: () {
+        context.read<EmployeesProvider>().clearEmployeeData();
         Navigator.pushNamed(context, NewEmployeeView.routeName);
       },
     );
--- lib/features/employees/presentation/views/widgets/new_employee_form.dart
+++ lib/features/employees/presentation/views/widgets/new_employee_form.dart
@@ -1,6 +1,11 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
+import 'package:provider/provider.dart';
 
+import '../../../../../core/functions/loading_dialog.dart';
+import '../../../../../core/helpers/app_message.dart';
 import '../../../../../core/utils/app_colors.dart';
 import '../../../../../core/utils/app_text_styles.dart';
 import '../../../../../core/widgets/custom_button.dart';
@@ -11,69 +16,103 @@ import '../../../../../core/widgets/custom_password_field.dart';
 import '../../../../../core/widgets/custom_phone_text_filed.dart';
 import '../../../../../core/widgets/custom_white_box.dart';
 import '../../../../../core/widgets/labeled_checkbox.dart';
+import '../../manager/employees_provider.dart';
 
 class NewEmployeeForm extends StatelessWidget {
   const NewEmployeeForm({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var prov = context.watch<EmployeesProvider>();
     return SingleChildScrollView(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Center(
         child: ConstrainedBox(
           constraints: const BoxConstraints(maxWidth: 555),
 
-          child: Column(
-            spacing: 16,
-            children: [
-              const SizedBox(height: 8),
-              _ProfileImage(),
-              const SizedBox(height: 8),
-              Row(
-                children: [
-                  Expanded(
-                    child: CustomNameField(
-                      labelText: 'الإسم الأول',
-                      hintText: 'الإسم الأول',
+          child: Form(
+            key: prov.formKey,
+            child: Column(
+              spacing: 16,
+              children: [
+                const SizedBox(height: 8),
+                _ProfileImage(),
+                const SizedBox(height: 8),
+                Row(
+                  children: [
+                    Expanded(
+                      child: CustomNameField(
+                        labelText: 'الإسم الأول',
+                        hintText: 'الإسم الأول',
+                        controller: prov.fnameController,
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                    child: CustomNameField(
-                      labelText: 'الإسم الثاني',
-                      hintText: 'الإسم الثاني',
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: CustomNameField(
+                        labelText: 'الإسم الثاني',
+                        hintText: 'الإسم الثاني',
+                        controller: prov.lnameController,
+                      ),
                     ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
 
-              CustomEmailField(
-                labelText: 'البريد الإلكتروني',
-                hintText: 'البريد الإلكتروني',
-              ),
+                CustomEmailField(
+                  labelText: 'البريد الإلكتروني',
+                  hintText: 'البريد الإلكتروني',
+                  controller: prov.emailController,
+                ),
 
-              CustomPhoneTextField(
-                labelText: 'رقم الهاتف',
-                hintText: 'رقم الهاتف',
-              ),
+                CustomPhoneTextField(
+                  labelText: 'رقم الهاتف',
+                  hintText: 'رقم الهاتف',
+                  controller: prov.phoneController,
+                ),
 
-              CustomPasswordField(
-                labelText: 'كلمة المرور',
-                hintText: 'كلمة المرور',
-              ),
+                CustomPasswordField(
+                  labelText: 'كلمة المرور',
+                  hintText: 'كلمة المرور',
+                  controller: prov.passwordController,
+                ),
 
-              _Permissions(),
+                _Permissions(),
 
-              const SizedBox(height: 16),
-              CustomButton(
-                text: 'إضافة موظف',
-                horizontalPadding: 75,
-                color: AppColors.sandyBrown,
-                onPressed: () {},
-              ),
+                const SizedBox(height: 16),
+                CustomButton(
+                  text: 'إضافة موظف',
+                  horizontalPadding: 75,
+                  color: AppColors.sandyBrown,
+                  onPressed: () async {
+                    if (prov.formKey.currentState!.validate()) {
+                      if (prov.pickedImage == null) {
+                        AppMessage.errorBar(context, message: 'الصورة مطلوبة');
+                        return;
+                      }
 
-              const SizedBox(height: 16),
-            ],
+                      //* show loading dialog
+                      loadingDialog(context);
+
+                      await prov.addNewEmployee();
+
+                      //* close loading dialog
+                      Navigator.pop(context);
+
+                      if (prov.checkAddingNewEmployee == true) {
+                        //* Close add-employee-view
+                        Navigator.pop(context);
+
+                        AppMessage.successBar(context, message: prov.message);
+                      } else if (prov.checkAddingNewEmployee == false) {
+                        AppMessage.errorBar(context, message: prov.message);
+                      }
+                    }
+                  },
+                ),
+
+                const SizedBox(height: 16),
+              ],
+            ),
           ),
         ),
       ),
@@ -86,6 +125,7 @@ class _ProfileImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var prov = context.watch<EmployeesProvider>();
     return SizedBox(
       height: 200,
       width: 200,
@@ -98,20 +138,31 @@ class _ProfileImage extends StatelessWidget {
               shape: BoxShape.circle,
               color: AppColors.white,
               border: Border.all(color: AppColors.borderGrey, width: 2),
-              // image: const DecorationImage(
-              //   image: AssetImage(Assets.imagesTestUser),
-              //   fit: BoxFit.cover,
-              // ),
+              image: prov.pickedImage == null
+                  ? null
+                  : DecorationImage(
+                      image: MemoryImage(prov.pickedImage!.bytes),
+                      fit: BoxFit.cover,
+                    ),
             ),
-            child: Icon(LucideIcons.user, color: AppColors.sandyBrown),
+            child: Visibility(
+              visible: prov.pickedImage == null,
+              child: Icon(LucideIcons.user, color: AppColors.sandyBrown),
+            ),
           ),
 
           Positioned(
             bottom: 0,
             right: 32,
             child: CustomCircularButton(
-              icon: LucideIcons.penLine,
-              onPressed: () {},
+              icon: prov.pickedImage == null
+                  ? LucideIcons.penLine
+                  : LucideIcons.x,
+              onPressed: () {
+                (prov.pickedImage == null)
+                    ? prov.onPickImage()
+                    : prov.onClearImage();
+              },
             ),
           ),
         ],
@@ -125,6 +176,7 @@ class _Permissions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var prov = context.watch<EmployeesProvider>();
     return CustomWhiteBox(
       color: AppColors.whiteGrey,
       child: Column(
@@ -143,28 +195,38 @@ class _Permissions extends StatelessWidget {
           const SizedBox(height: 12),
           LabeledCheckbox(
             labelText: 'إدارة الرحلات السياحية',
-            value: false,
-            onChanged: (value) {},
+            value: prov.permissions.manageTrips,
+            onChanged: (value) {
+              prov.onChangePermission(manageTrips: value);
+            },
           ),
           LabeledCheckbox(
             labelText: 'إدارة الرحلات الموقوفة',
-            value: false,
-            onChanged: (value) {},
+            value: prov.permissions.manageSuspendedTrips,
+            onChanged: (value) {
+              prov.onChangePermission(manageSuspendedTrips: value);
+            },
           ),
           LabeledCheckbox(
             labelText: 'إدارة المستخدمين',
-            value: false,
-            onChanged: (value) {},
+            value: prov.permissions.manageUsers,
+            onChanged: (value) {
+              prov.onChangePermission(manageUsers: value);
+            },
           ),
           LabeledCheckbox(
             labelText: 'إدارة طلبات الحجز',
-            value: false,
-            onChanged: (value) {},
+            value: prov.permissions.manageBookingRequests,
+            onChanged: (value) {
+              prov.onChangePermission(manageBookingRequests: value);
+            },
           ),
           LabeledCheckbox(
             labelText: 'إدارة الموقع الإلكتروني',
-            value: false,
-            onChanged: (value) {},
+            value: prov.permissions.manageWebsite,
+            onChanged: (value) {
+              prov.onChangePermission(manageWebsite: value);
+            },
           ),
         ],
       ),
