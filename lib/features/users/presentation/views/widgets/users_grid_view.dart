39367c71503aaa013ce0ec1360819be078be68ab
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-02-18 13:14:05 -0800

    Show users part one.

--- lib/features/users/presentation/views/widgets/users_grid_view.dart
+++ lib/features/users/presentation/views/widgets/users_grid_view.dart
@@ -1,8 +1,15 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
+import '../../../../../core/models/user_model.dart';
+import '../../../../../core/utils/assets.dart';
+import '../../../../../core/widgets/api_error_widget.dart';
 import '../../../../../core/widgets/custom_person_card.dart';
+import '../../../../../core/widgets/empty_grid_widget.dart';
+import '../../manager/users_provider.dart';
 import '../user_details_view.dart';
 
 class UsersGridView extends StatelessWidget {
@@ -10,34 +17,59 @@ class UsersGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var prov = context.watch<UsersProvider>();
+
+    List<UserModel> loadingList = List.generate(
+      8,
+      (index) => UserModel.empty(),
+    );
+
+    List<UserModel> users = prov.checkGettingAllUser == null
+        ? loadingList
+        : prov.users;
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: LayoutBuilder(
         builder: (context, constraints) {
           var width = constraints.maxWidth;
           var crossAxisCount = (width / 210).toInt();
-          return GridView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: max(crossAxisCount, 1),
-              mainAxisSpacing: 12.0,
-              crossAxisSpacing: 12.0,
-              childAspectRatio: 221 / 386,
-              mainAxisExtent: 386,
-            ),
-            itemCount: 20,
-            itemBuilder: (context, index) {
-              return CustomPersonCard(
-                name: 'محمد خالد ابن سلمان',
-                phone: '+20 0109876543',
-                email: 'a7med123@gmail.com',
-                onTap: () {
-                  Navigator.pushNamed(context, UserDetailsView.routeName);
-                },
-              );
-            },
-          );
+          return prov.checkGettingAllUser == false
+              ? ApiErrorView(msg: prov.message, onRetry: prov.getAllUsers)
+              : users.isEmpty
+              ? EmptyGridWidget(
+                  lottie: Assets.animationsEmptyGrid2,
+                  message: 'لا يوجد رحلات',
+                )
+              : Skeletonizer(
+                  enabled: prov.checkGettingAllUser == null,
+                  child: GridView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: max(crossAxisCount, 1),
+                      mainAxisSpacing: 12.0,
+                      crossAxisSpacing: 12.0,
+                      childAspectRatio: 221 / 386,
+                      mainAxisExtent: 386,
+                    ),
+                    itemCount: users.length,
+                    itemBuilder: (context, index) {
+                      return CustomPersonCard(
+                        name: users[index].fullName,
+                        phone: users[index].phone,
+                        email: users[index].email,
+                        imgUrl: users[index].image,
+                        onTap: () {
+                          Navigator.pushNamed(
+                            context,
+                            UserDetailsView.routeName,
+                          );
+                        },
+                      );
+                    },
+                  ),
+                );
         },
       ),
     );
