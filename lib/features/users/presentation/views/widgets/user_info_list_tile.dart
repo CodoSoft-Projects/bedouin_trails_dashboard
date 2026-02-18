4ee4439d1d74db1fde2a7f16d9c10b49dadfd134
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-02-18 13:29:17 -0800

    Show users part three in user-details-view.

--- lib/features/users/presentation/views/widgets/user_info_list_tile.dart
+++ lib/features/users/presentation/views/widgets/user_info_list_tile.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../../../../core/models/user_model.dart';
 import '../../../../../core/utils/app_text_styles.dart';
-import '../../../../../core/utils/assets.dart';
+import '../../../../../core/widgets/custom_cached_network_image.dart';
+import '../../manager/users_provider.dart';
 
 class UserInfoListTile extends StatelessWidget {
   const UserInfoListTile({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var prov = context.watch<UsersProvider>();
+    var user = prov.selectedUser ?? UserModel.empty();
     return ListTile(
       contentPadding: EdgeInsets.zero,
-      title: Text('محمد خالد محمود', style: AppTextStyles.bold22(context)),
-      subtitle: Text('+20109876543', style: AppTextStyles.regular16(context)),
+      title: Text(user.fullName, style: AppTextStyles.bold22(context)),
+      subtitle: Text(user.phone, style: AppTextStyles.regular16(context)),
       leading: CircleAvatar(
         radius: 20,
-        backgroundImage: AssetImage(Assets.imagesTestUser),
+        backgroundImage: customCachedNetworkImageprovider(user.image),
       ),
     );
   }
