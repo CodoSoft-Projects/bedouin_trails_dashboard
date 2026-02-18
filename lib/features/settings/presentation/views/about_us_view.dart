f7f27552ef0ca87dab1985ea8a49da870a12bc52
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-02-18 08:36:05 -0800

    About-Us feature part one [UI].

---
+++ lib/features/settings/presentation/views/about_us_view.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../core/widgets/api_error_widget.dart';
+import '../../../../core/widgets/custom_app_bar.dart';
+import '../manager/about_us_provider.dart';
+import 'widgets/article_form_section.dart';
+import 'widgets/articles_section.dart';
+import 'widgets/no_articles_section.dart';
+
+class AboutUsView extends StatefulWidget {
+  const AboutUsView({super.key});
+  static const String routeName = '/about-us-view';
+
+  @override
+  State<AboutUsView> createState() => _AboutUsViewState();
+}
+
+class _AboutUsViewState extends State<AboutUsView> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      // ignore: use_build_context_synchronously
+      context.read<AboutUsProvider>().getAllItems();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: customAppBar(context, title: 'إدارة صفحة من نحن'),
+      body: const AboutUsViewBody(),
+    );
+  }
+}
+
+class AboutUsViewBody extends StatelessWidget {
+  const AboutUsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var prov = context.watch<AboutUsProvider>();
+    if (prov.checkGettingItems == false) {
+      return ApiErrorView(msg: prov.message, onRetry: prov.getAllItems);
+    }
+    if (prov.items.isEmpty && prov.checkGettingItems == true) {
+      return const NoArticlesSection();
+    }
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        spacing: 12,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(flex: 3, child: ArticlesSection()),
+          Expanded(flex: 5, child: ArticleFormSection()),
+        ],
+      ),
+    );
+  }
+}
