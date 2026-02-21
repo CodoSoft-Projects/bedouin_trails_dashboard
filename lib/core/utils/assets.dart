0d16d687edcd9cc6684263e93d957b22b14c2983
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2026-02-21 08:44:49 -0800

    Create Panel-Header-Entity & create list from it.

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -14,15 +14,25 @@ class Assets {
 
   static const String imagesCamelCircle = "assets/images/camel_circle.png";
 
+  static const String imagesCancelIcon = "assets/images/cancel_icon.png";
+
   static const String imagesEmployeesIcon = "assets/images/employees_icon.png";
 
   static const String imagesForgetImage = "assets/images/forget_image.png";
+
+  static const String imagesGrownIcon = "assets/images/grown_icon.png";
 
   static const String imagesLoginImage = "assets/images/login_image.png";
 
   static const String imagesLogo = "assets/images/logo.png";
 
+  static const String imagesNewBookingIcon = "assets/images/new_booking_icon.png";
+
   static const String imagesProfileBackground = "assets/images/profile_background.png";
+
+  static const String imagesRattingIcon = "assets/images/ratting_icon.png";
+
+  static const String imagesReportsIcon = "assets/images/reports_icon.png";
 
   static const String imagesResetImage = "assets/images/reset_image.png";
 
@@ -33,5 +43,7 @@ class Assets {
   static const String imagesTestUser2 = "assets/images/test_user2.png";
 
   static const String imagesTripsIcon = "assets/images/trips_icon.png";
+
+  static const String imagesUsersIcon = "assets/images/users_icon.png";
 }
 
---
+++ lib/features/control_panel/domain/entities/panel_header_entity.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/assets.dart';
+import '../../../../core/utils/app_colors.dart';
+
+class PanelHeaderEntity {
+  final String title;
+  final String subtitle;
+  final String image;
+  final Color backgroundColor;
+
+  PanelHeaderEntity({
+    required this.title,
+    required this.subtitle,
+    required this.image,
+    this.backgroundColor = AppColors.whiteGrey,
+  });
+}
+
+List<PanelHeaderEntity> panelHeadersList = [
+  PanelHeaderEntity(
+    title: 'حجوزات جديدة لليوم',
+    subtitle: '12',
+    image: Assets.imagesNewBookingIcon,
+  ),
+  PanelHeaderEntity(
+    title: 'عدد المستخدمين',
+    subtitle: '800',
+    image: Assets.imagesUsersIcon,
+  ),
+  PanelHeaderEntity(
+    title: 'عدد المقالات في الموقع',
+    subtitle: '24',
+    image: Assets.imagesReportsIcon,
+  ),
+];
+
+List<PanelHeaderEntity> panelBottomList = [
+  PanelHeaderEntity(
+    title: 'معدل التقييمات',
+    subtitle: '4.5 / 5',
+    image: Assets.imagesRattingIcon,
+    backgroundColor: AppColors.white,
+  ),
+  PanelHeaderEntity(
+    title: 'معدل إلغاء الحجوزات',
+    subtitle: '6 %',
+    image: Assets.imagesCancelIcon,
+    backgroundColor: AppColors.white,
+  ),
+  PanelHeaderEntity(
+    title: 'معدل زيادة المستخدمين الجدد',
+    image: Assets.imagesUsersIcon,
+    subtitle: '84 %',
+    backgroundColor: AppColors.white,
+  ),
+];
+
+PanelHeaderEntity grownItem = PanelHeaderEntity(
+  title: 'نسبة نمو الواردات للشهر الحالي',
+  subtitle: '12 %',
+  image: Assets.imagesGrownIcon,
+  backgroundColor: AppColors.white,
+);
